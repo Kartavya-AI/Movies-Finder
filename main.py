edeafdbac36ff6bdeae28773c096ba795af2fba8
Rajani-07 <drajani3379@gmail.com>
2025-08-14 20:47:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import streamlit as st
 async def main():
     # Load environment variables
     load_dotenv()
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    LANGSMITH_API_KEY = os.getenv('LANGSMITH_API_KEY')
+    os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+    os.environ['LANGSMITH_API_KEY'] = os.getenv('LANGSMITH_API_KEY')
     os.environ['LANGSMITH_TRACING'] = 'true'
     os.environ['LANGCHAIN_TRACING_V2'] = 'true'
     os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
@@ -26,7 +26,7 @@ async def main():
     if "client" not in st.session_state:
         st.session_state.client = MCPClient.from_config_file(config_file)
     if "llm" not in st.session_state:
-        st.session_state.llm = ChatOpenAI(model="gpt-3.5-turbo",base_url="https://openrouter.ai/api/v1",api_key=OPEN_API_KEY,max_tokens=200,temperature=0.7)
+        st.session_state.llm = ChatOpenAI(model="gpt-3.5-turbo",base_url="https://openrouter.ai/api/v1",max_tokens=200,temperature=0.7)
     if "agent" not in st.session_state:
         st.session_state.agent = MCPAgent(llm=st.session_state.llm, client=st.session_state.client, max_steps=5, memory_enabled=True)
         st.session_state.agent.set_system_message(
