ca0b96e2ec10cd8ef3fc311986f2affb1b960b78
Rajani-07 <drajani3379@gmail.com>
2025-08-15 00:09:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ async def main():
     os.environ['LANGUAGE'] = 'en-US'
 
     config_file = "browser_mcp.json"
+    if not OPENAI_API_KEY:
+        raise ValueError("OPENAI_API_KEY not found in environment variables")
+
 
     print("Initializing chat...")
     
@@ -26,7 +29,7 @@ async def main():
     if "client" not in st.session_state:
         st.session_state.client = MCPClient.from_config_file(config_file)
     if "llm" not in st.session_state:
-        st.session_state.llm = ChatOpenAI(model="gpt-3.5-turbo",base_url="https://openrouter.ai/api/v1",max_tokens=200,temperature=0.7)
+        st.session_state.llm = ChatOpenAI(model="gpt-3.5-turbo",base_url="https://openrouter.ai/api/v1",api_key=OPENAI_API_KEY,max_tokens=200,temperature=0.7)
     if "agent" not in st.session_state:
         st.session_state.agent = MCPAgent(llm=st.session_state.llm, client=st.session_state.client, max_steps=5, memory_enabled=True)
         st.session_state.agent.set_system_message(
