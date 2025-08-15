f954e6ac58d3a8560d2fbdec90dc0418c726950f
Rajani-07 <drajani3379@gmail.com>
2025-08-15 11:23:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def main():
     if "llm" not in st.session_state:
         st.session_state.llm = ChatOpenAI(model="gpt-3.5-turbo",base_url="https://openrouter.ai/api/v1",api_key=OPENAI_API_KEY,max_tokens=200,temperature=0.7)
     if "agent" not in st.session_state:
-        st.session_state.agent = MCPAgent(llm=st.session_state.llm, client=st.session_state.client, max_steps=5, memory_enabled=True)
+        st.session_state.agent = MCPAgent(llm=st.session_state.llm, client=st.session_state.client, max_steps=20, memory_enabled=True)
         st.session_state.agent.set_system_message(
             "You are an assistant. Always respond strictly in English. "
             "If you receive or retrieve any content in another language, "
